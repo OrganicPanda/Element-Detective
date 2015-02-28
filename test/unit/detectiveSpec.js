5c18f67d2cd476e13a2a2beaacab73aabfd5497b
Steve Thomson <organicpanda@gmail.com>
2015-02-28 22:09:16 +0000

    A real test !

--- test/unit/detectiveSpec.js
+++ test/unit/detectiveSpec.js
@@ -1,8 +1,22 @@
 'use strict';
 
-describe('Detective', function() {
+describe('detective', function() {
+  var setup = function(html) {
+    var container = document.createElement('div');
+
+    container.innerHTML = html;
+
+    return container.firstChild;
+  };
+
   it('runs', function() {
-    //console.log('elTextPos', elTextPos);
-    expect(1).toBe(1);
+    var el = setup('<div><p>Foo</p><p>Bar</p></div>')
+      , child = el.querySelector('p:nth-child(2)');
+
+    expect(elTextPosFoo(child, el)).toEqual({
+      start: 15,
+      end: 25,
+      length: 10
+    });
   });
 });
